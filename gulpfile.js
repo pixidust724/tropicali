d3040422e172551bbc1ef17ee4ef19678aed8f7c
pixidust724 <pixidust724@gmail.com>
2019-01-09 00:52:34 +0800

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var sourcemaps = require('gulp-sourcemaps')
 var browserSync = require('browser-sync').create()
 
 var imagemin = require("gulp-imagemin")
+var ghpages = require("gh-pages")
+
 
 sass.compiler = require('node-sass');
 
@@ -58,5 +60,8 @@ gulp.task("watch", function () {
   gulp.watch("src/img/*", ["images"])
 })
 
+gulp.task("deploy", function (){
+  ghpages.publish("dist")
+})
 
 gulp.task('default', ["html" ,"sass", "fonts", "images", "watch"]);
